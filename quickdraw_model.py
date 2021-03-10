5cadf1ca75b102afa7ebfd59792f84c52ed5d1b3
Ketaki Hadnurkar <ketaki.hadnurkar@gmail.com>
2021-03-10 17:53:51 +0530

    Images resized.

--- quickdraw_model.py
+++ quickdraw_model.py
@@ -8,9 +8,9 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import random
-from PIL import Image
+from PIL import Image,ImageOps
 import os
-
+# from tqdm import tqdm
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 
@@ -37,10 +37,14 @@ class customDatasetClass(Dataset):
     def __getitem__(self, item):
 
         image = Image.open(self.allImagePaths[item]).convert('RGB')
+        image1 = ImageOps.grayscale(image) 
+        image1=image1.resize((32,32))
+        # image1.show()
+        # exit(0)
         target = self.allTargets[item]
-        image = self.transforms(image)
-   
-        return image, target
+        image1 = self.transforms(image1)
+
+        return image1, target
 
     def __len__(self):
 
@@ -49,32 +53,34 @@ class customDatasetClass(Dataset):
 class Net(nn.Module):
     def __init__(self):
         super(Net,self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.conv3 = nn.Conv2d(64, 32, 3, 1)
+        self.conv1 = nn.Conv2d(1, 32, 2, 1)
+        self.conv2 = nn.Conv2d(32, 64, 2, 1)
+        # self.conv3 = nn.Conv2d(64, 128, 2, 1)
         # self.conv4 = nn.Conv2d(96, 64, 3, 1)
-        self.conv5 = nn.Conv2d(32, 16, 3, 1)
+        self.conv3 = nn.Conv2d(64, 32, 2, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(7744, 400)
-        self.fc2 = nn.Linear(400, 10)
+        self.fc1 = nn.Linear(6272, 100)
+        self.fc2 = nn.Linear(100, 10)
 
     def forward(self, x):
+        # print(x.shape)
+        # exit(0)
         x = self.conv1(x)
         x = F.relu(x)
-        x = F.max_pool2d(x, 2)
+        # x = F.max_pool2d(x, 2)
         # print(x.shape)
         # exit(0)
         x = self.conv2(x)
         x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.conv3(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
+        # x = F.max_pool2d(x, 2)
+        # x = self.conv3(x)
+        # x = F.relu(x)
+        # x = F.max_pool2d(x, 2)
         # x = self.conv4(x)
         # x = F.relu(x)
         # x = F.max_pool2d(x, 2)
-        x = self.conv5(x)
+        x = self.conv3(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
         
@@ -98,13 +104,13 @@ def train(model, use_cuda, train_loader, optimizer, epoch):
             data, target = data.cuda(), target.cuda()  # Sending the data to the GPU
 
         optimizer.zero_grad()  # Setting the cumulative gradients to 0
-        output = model(data)
+        output = model(data.float())
         # print(output.shape,target.shape)# Forward pass through the model
         loss = F.cross_entropy(output,target.long())  # Calculating the loss
         loss.backward()  # Calculating the gradients of the model. Note that the model has not yet been updated.
         optimizer.step()  # Updating the model parameters. Note that this does not remove the stored gradients!
 
-        if batch_idx % 10 == 0:
+        if batch_idx % 100 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
@@ -124,7 +130,7 @@ def test(model, use_cuda, test_loader):
 
             # argmax([0.1, 0.2, 0.9, 0.4]) => 2
             # output - shape = [1000, 10], argmax(dim=1) => [1000]
-            output = model(data.float())  # Forward pass
+            output = model(data)  # Forward pass
             test_loss += F.cross_entropy(output,target.long(),reduction='sum')  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the maximum output
             correct += pred.eq(target.view_as(pred)).sum().item()  # Get total number of correct samples
@@ -159,8 +165,8 @@ def main():
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
     trainloader = DataLoader(
-        customDatasetClass('C:/Users\DELL\Desktop\ML\Machine-Learning\GoogleDataImages_train'),
-        batch_size=60,
+        customDatasetClass('C:/Users/DELL/Desktop/ML/Machine-Learning/GoogleDataImages_train'),
+        batch_size=64,
         num_workers=2,
         shuffle=True
     )
@@ -169,7 +175,7 @@ def main():
     optimizer = optim.SGD(model.parameters(), lr=0.001)  # Choose the optimizer and the set the learning rate
     testloader = DataLoader(
         customDatasetClass('C:/Users\DELL\Desktop\ML\Machine-Learning\GoogleDataImages_test'),
-        batch_size=60,
+        batch_size=64,
         num_workers=2,
         shuffle=True
     )
@@ -178,9 +184,9 @@ def main():
         test(model, use_cuda, testloader)  # Test the network
 
 
-    torch.save(model.state_dict(), "cifar.pt")
+    torch.save(model.state_dict(), "quickdraw_model.pt")
 
-    model.load_state_dict(torch.load('cifar.pt'))
+    model.load_state_dict(torch.load('quickdraw_model.pt'))
 
 if __name__ == "__main__":
     main()
