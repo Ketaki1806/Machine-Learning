4e860f117535a8fc9c998a198abfc16afbc5830e
Ketaki Hadnurkar <ketaki.hadnurkar@gmail.com>
2021-03-12 22:05:05 +0530

    81% accuracy

--- quickdraw_model.py
+++ quickdraw_model.py
@@ -8,9 +8,8 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import random
-from PIL import Image,ImageOps
+from PIL import Image
 import os
-# from tqdm import tqdm
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 
@@ -27,24 +26,18 @@ class customDatasetClass(Dataset):
             for imageI in sorted(os.listdir(self.path + '/' + targetI + '/image')):
                 self.allImagePaths.append(self.path + '/' + targetI + '/image/' + imageI)
                 self.allTargets.append(targetNo)
-
         self.transforms = torchvision.transforms.Compose([
             # torchvision.transforms.RandomCrop((256, 256)),
             torchvision.transforms.ToTensor()
         ])
-        # allImagePaths=allImagePaths.reshape(allImagePaths.shape[0],3,32,32)
 
     def __getitem__(self, item):
 
-        image = Image.open(self.allImagePaths[item]).convert('RGB')
-        image1 = ImageOps.grayscale(image) 
-        image1=image1.resize((32,32))
-        # image1.show()
-        # exit(0)
+        image = Image.fromarray(np.float32(np.array(Image.fromarray(plt.imread(self.allImagePaths[item])[:, :, 3]).resize((64, 64))) > 0))
         target = self.allTargets[item]
-        image1 = self.transforms(image1)
+        image = self.transforms(image)
 
-        return image1, target
+        return image, target
 
     def __len__(self):
 
@@ -55,43 +48,37 @@ class Net(nn.Module):
         super(Net,self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 2, 1)
         self.conv2 = nn.Conv2d(32, 64, 2, 1)
-        # self.conv3 = nn.Conv2d(64, 128, 2, 1)
-        # self.conv4 = nn.Conv2d(96, 64, 3, 1)
         self.conv3 = nn.Conv2d(64, 32, 2, 1)
+        self.conv4 = nn.Conv2d(32, 16, 3, 1)
+        # self.conv3 = nn.Conv2d(64, 32, 2, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(6272, 100)
-        self.fc2 = nn.Linear(100, 10)
+        self.fc1 = nn.Linear(64, 10)
+        # self.fc2 = nn.Linear(0, 10)
 
     def forward(self, x):
-        # print(x.shape)
-        # exit(0)
         x = self.conv1(x)
         x = F.relu(x)
-        # x = F.max_pool2d(x, 2)
-        # print(x.shape)
-        # exit(0)
+        x = F.max_pool2d(x, 2)
         x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv4(x)
         x = F.relu(x)
         # x = F.max_pool2d(x, 2)
         # x = self.conv3(x)
         # x = F.relu(x)
-        # x = F.max_pool2d(x, 2)
-        # x = self.conv4(x)
-        # x = F.relu(x)
-        # x = F.max_pool2d(x, 2)
-        x = self.conv3(x)
-        x = F.relu(x)
         x = F.max_pool2d(x, 2)
         
-        x = self.dropout1(x)
-        x = x.view(x.shape[0],-1)
-        # print(x.shape)
-        # exit(0)
-        x = self.fc1(x)
-        x = F.relu(x)
         x = self.dropout2(x)
-        x = self.fc2(x)
+        x = x.view(x.shape[0],-1)
+        x = self.fc1(x)
+        # x = F.relu(x)
+        # x = self.dropout2(x)
+        # x = self.fc2(x)
         return x
 
 def train(model, use_cuda, train_loader, optimizer, epoch):
@@ -104,9 +91,8 @@ def train(model, use_cuda, train_loader, optimizer, epoch):
             data, target = data.cuda(), target.cuda()  # Sending the data to the GPU
 
         optimizer.zero_grad()  # Setting the cumulative gradients to 0
-        output = model(data.float())
-        # print(output.shape,target.shape)# Forward pass through the model
-        loss = F.cross_entropy(output,target.long())  # Calculating the loss
+        output = model(data.float()) # Forward pass through the model
+        loss = F.cross_entropy(output,target)  # Calculating the loss
         loss.backward()  # Calculating the gradients of the model. Note that the model has not yet been updated.
         optimizer.step()  # Updating the model parameters. Note that this does not remove the stored gradients!
 
@@ -170,23 +156,24 @@ def main():
         num_workers=2,
         shuffle=True
     )
-
     model=Net()
-    optimizer = optim.SGD(model.parameters(), lr=0.001)  # Choose the optimizer and the set the learning rate
+    optimizer = optim.Adam(model.parameters(), lr=0.001)  # Choose the optimizer and the set the learning rate
     testloader = DataLoader(
         customDatasetClass('C:/Users\DELL\Desktop\ML\Machine-Learning\GoogleDataImages_test'),
         batch_size=64,
         num_workers=2,
         shuffle=True
     )
-    for epoch in range(1, 10 + 1):
+    for epoch in range(1, 15 + 1):
         train(model, use_cuda, trainloader, optimizer, epoch)  # Train the network
         test(model, use_cuda, testloader)  # Test the network
 
-
-    torch.save(model.state_dict(), "quickdraw_model.pt")
-
-    model.load_state_dict(torch.load('quickdraw_model.pt'))
+    os.makedirs("data_model", exist_ok=True)
+    os.makedirs("save_model", exist_ok=True)
+    torch.save(model.state_dict(), "data_model/quickdraw_cnn_epoch.pt")
+    dummy_input=torch.zeros(1,1,64,64)
+    torch.onnx.export(model,dummy_input,"save_model/model.onnx", verbose = True, input_names = ['data'], output_names = ['output'])
+    model.load_state_dict(torch.load('data_model/quickdraw_cnn_epoch.pt'))
 
 if __name__ == "__main__":
     main()
